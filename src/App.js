4e936d3586d6c4cbc25b2b4551ebe2afa4cf4d82
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-08-26 12:34:10 +0300

    Clear Code

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { AnimatedSwitch, spring } from 'react-router-transition';
+import { AnimatedSwitch } from 'react-router-transition';
+import { mapStyles, bounceTransition } from './animation';
 
-import { Sidemenu } from './components';
-import { Dashboard } from './components';
+import { Sidemenu, Dashboard } from './components';
 
 import Messenger from './components/pages/Messenger';
 import Projects from './components/pages/Projects';
 import Analytics from './components/pages/Analytics';
 import Team from './components/pages/Team';
 import Settings from './components/pages/Settings';
-
-// we need to map the `scale` prop we define below
-// to the transform style property
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`,
-  };
-}
-
-// wrap the `spring` helper to use a bouncy config
-function bounce(val) {
-  return spring(val, {
-    stiffness: 330,
-    damping: 22,
-  });
-}
-
-// child matches will...
-const bounceTransition = {
-  // start in a transparent, upscaled state
-  atEnter: {
-    opacity: 0,
-    scale: 1.2,
-  },
-  // leave in a transparent, downscaled state
-  atLeave: {
-    opacity: bounce(0),
-    scale: bounce(0.8),
-  },
-  // and rest at an opaque, normally-scaled state
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1),
-  },
-};
 
 function App() {
   return (
